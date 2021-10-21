3bf6a39f770b6cc863b309d21f5bb97ce709ee62
Hector <vv96rca@gmail.com>
2021-10-21 13:39:14 +0530

    added components

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
-export default class Calculator extends Component {
+class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      operation: '',
+      next: '',
+    };
   }
 
+  update = (e) => { this.setState((object) => calculate(object, e.target.textContent)); }
+
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="container">
         <div className="screen">
-          0
+          {` ${
+            total || operation || next
+              ? `${total || ''} ${operation || ''} ${next || ''}`
+              : '0'
+          }`}
         </div>
         <div className="buttons">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="orange" type="button">&divide;</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="orange" type="button">&times;</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="orange" type="button">&minus;</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="orange" type="button">+</button>
-          <button className="double" type="button">0</button>
-          <button type="button">.</button>
-          <button className="orange" type="button">=</button>
+          <button onClick={this.update} type="button">AC</button>
+          <button onClick={this.update} type="button">+/-</button>
+          <button onClick={this.update} type="button">%</button>
+          <button onClick={this.update} className="orange" type="button">÷</button>
+          <button onClick={this.update} type="button">7</button>
+          <button onClick={this.update} type="button">8</button>
+          <button onClick={this.update} type="button">9</button>
+          <button onClick={this.update} className="orange" type="button">x</button>
+          <button onClick={this.update} type="button">4</button>
+          <button onClick={this.update} type="button">5</button>
+          <button onClick={this.update} type="button">6</button>
+          <button onClick={this.update} className="orange" type="button">-</button>
+          <button onClick={this.update} type="button">1</button>
+          <button onClick={this.update} type="button">2</button>
+          <button onClick={this.update} type="button">3</button>
+          <button onClick={this.update} className="orange" type="button">+</button>
+          <button onClick={this.update} className="double" type="button">0</button>
+          <button onClick={this.update} type="button">.</button>
+          <button onClick={this.update} className="orange" type="button">=</button>
         </div>
       </div>
     );
   }
 }
+
+export default Calculator;
