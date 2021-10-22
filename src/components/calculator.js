b51d67211fbdcc0eb2655d3342cd8542977c3468
Hector <vv96rca@gmail.com>
2021-10-22 15:12:15 +0530

    refactored with hooks and fucntional component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,54 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: '',
-      next: '',
-    };
-  }
+export default function Calculator() {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const update = (e) => {
+    const obj = calculate(state, e.target.innerText);
+    setState(obj);
+  };
 
-  update = (e) => { this.setState((object) => calculate(object, e.target.textContent)); }
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="container">
-        <div className="screen">
-          {` ${
-            total || operation || next
-              ? `${total || ''} ${operation || ''} ${next || ''}`
-              : '0'
-          }`}
-        </div>
-        <div className="buttons">
-          <button onClick={this.update} type="button">AC</button>
-          <button onClick={this.update} type="button">+/-</button>
-          <button onClick={this.update} type="button">%</button>
-          <button onClick={this.update} className="orange" type="button">÷</button>
-          <button onClick={this.update} type="button">7</button>
-          <button onClick={this.update} type="button">8</button>
-          <button onClick={this.update} type="button">9</button>
-          <button onClick={this.update} className="orange" type="button">x</button>
-          <button onClick={this.update} type="button">4</button>
-          <button onClick={this.update} type="button">5</button>
-          <button onClick={this.update} type="button">6</button>
-          <button onClick={this.update} className="orange" type="button">-</button>
-          <button onClick={this.update} type="button">1</button>
-          <button onClick={this.update} type="button">2</button>
-          <button onClick={this.update} type="button">3</button>
-          <button onClick={this.update} className="orange" type="button">+</button>
-          <button onClick={this.update} className="double" type="button">0</button>
-          <button onClick={this.update} type="button">.</button>
-          <button onClick={this.update} className="orange" type="button">=</button>
-        </div>
+  return (
+    <div className="container">
+      <div className="screen">
+        {` ${
+          state.total || state.operation || state.next
+            ? `${state.total || ''} ${state.operation || ''} ${state.next || ''}`
+            : '0'
+        }`}
       </div>
-    );
-  }
+      <div className="buttons">
+        <button onClick={update} type="button">
+          AC
+        </button>
+        <button onClick={update} type="button">
+          +/-
+        </button>
+        <button onClick={update} type="button">
+          %
+        </button>
+        <button onClick={update} className="orange" type="button">
+          ÷
+        </button>
+        <button onClick={update} type="button">
+          7
+        </button>
+        <button onClick={update} type="button">
+          8
+        </button>
+        <button onClick={update} type="button">
+          9
+        </button>
+        <button onClick={update} className="orange" type="button">
+          x
+        </button>
+        <button onClick={update} type="button">
+          4
+        </button>
+        <button onClick={update} type="button">
+          5
+        </button>
+        <button onClick={update} type="button">
+          6
+        </button>
+        <button onClick={update} className="orange" type="button">
+          -
+        </button>
+        <button onClick={update} type="button">
+          1
+        </button>
+        <button onClick={update} type="button">
+          2
+        </button>
+        <button onClick={update} type="button">
+          3
+        </button>
+        <button onClick={update} className="orange" type="button">
+          +
+        </button>
+        <button onClick={update} className="double" type="button">
+          0
+        </button>
+        <button onClick={update} type="button">
+          .
+        </button>
+        <button onClick={update} className="orange" type="button">
+          =
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default Calculator;
